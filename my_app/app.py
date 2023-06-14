7480565fb7c9a91a5f4cfae7f6e6372fc6839e54
ArmandleRoux <arramband@gmail.com>
2023-06-14 22:00:54 +0200

    Changed login process and added data collection page

--- my_app/app.py
+++ my_app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request, redirect, g
+from flask import Flask, render_template, request, redirect, g, session
+from flask_session import Session
 import sqlite3
 import os
 from .db_manager import query_db
@@ -6,6 +7,10 @@ from hashlib import sha512
 
 main_app = Flask(__name__)
 
+main_app.config['SESSION_PERMANENT'] = False
+main_app.config['SESSION_TYPE'] = 'filesystem'
+
+Session(main_app)
 
 # Function to get current database
 def get_db():
@@ -29,13 +34,11 @@ def close_connection(exception):
         db.close()
 
 
-
-
-
-
 @main_app.route('/')
 def index():
-    return redirect('/register')
+    if not session.get('name'):
+        return redirect('/login')
+    return redirect('/home')
 
 
 # Register page allows user to add username and sport
@@ -48,10 +51,36 @@ def register():
 def login():
     return render_template('login.html')
 
+@main_app.route("/logout")
+def logout():
+    session['name'] = None
+    return redirect('/login')
+
+
+@main_app.route('/home')
+def home_page():
+    username = session.get('name')
+    return render_template('sign_in_home.html', username=username)
+
+
+@main_app.route('/submitlogin', methods=["POST"])
+def submit_login():
+    username = request.form.get('name')
+    password = request.form.get('password')
+    password = sha512(password.encode('utf-8')).hexdigest()
+    with main_app.app_context():
+        result = query_db(get_db(), "SELECT password FROM USERS WHERE username=?", [username], True)
+        print(password)
+        print(result)
+        if result and result[0] == password:       
+            session['name'] = username    
+            return redirect("/home")
+        return render_template('login_fail.html', message="Username in unavailable!")
+
 
 # Submit checks if username is already in database and return corresponding page.
-@main_app.route('/submit', methods=["POST"])
-def submit():
+@main_app.route('/submitregister', methods=["POST"])
+def submit_register():
     username = request.form.get('name')
     password = request.form.get('password')
     confirm_pw = request.form.get('conf_pw')
@@ -61,7 +90,7 @@ def submit():
     with main_app.app_context():
         try:
             query_db(get_db(), "INSERT INTO Users(username, password) VALUES(?,?)", (username, password))
-            return render_template('register_success.html', username=username)
+            return render_template('sign_in_home.html', message= "Register Login", username=username)
         except:
             return render_template('register_fail.html', message="Username in unavailable!")
         
@@ -80,6 +109,15 @@ def users():
         for i, user in enumerate(result):
             output[i] = [user[0], user[1]]
         return output
+    
+    
+@main_app.route('/edit_user_data')
+def edit_user_data():
+    username = session['name']
+    result = query_db(get_db(), "SELECT birth_date, colour FROM Users WHERE username=?", tuple(username), True)
+    birth_date = result[0]
+    colour = result[1]
+    return render_template('add_user_data.html')
     
     
 if __name__ == '__main__':
