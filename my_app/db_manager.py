a18e6b97f0a7918eb89a2c4036be73afc1bc355e
ArmandleRoux <arramband@gmail.com>
2023-06-16 19:21:46 +0200

    Added back buttons

--- my_app/db_manager.py
+++
@@ -1,6 +0,0 @@
-def query_db(db, query, args=(), one=False):
-    cur = db.execute(query, args)
-    result = cur.fetchall()
-    cur.close()
-    db.commit()
-    return (result[0] if result else None) if one else result
--- my_app/main_app.py
+++ my_app/main_app.py
@@ -47,7 +47,7 @@ def home_page():
 # Check user login data and log them in.
 @main_app.route('/submitlogin', methods=["POST"])
 def submit_login():
-    username = request.form.get('name')
+    username = request.form.get('username')
     password = request.form.get('password')
     password = sha512(password.encode('utf-8')).hexdigest()
     print(username)
@@ -57,31 +57,36 @@ def submit_login():
         session['name'] = username
         login_user(user) 
         return redirect("/home")
-    return render_template('login_fail.html', message="Login has failed!")
+    return render_template('login_fail.html', message="Login has failed!", fail_type='Login', route='/login')
 
 
 # Determine if user entered valid credential and 
 @main_app.route('/submitregister', methods=["POST"])
 def submit_register():
-    username = request.form.get('name')
+    username = request.form.get('username')
     password = request.form.get('password')
     confirm_pw = request.form.get('conf_pw')
     if password != confirm_pw or not auth_password(password):
-        return render_template('register_fail.html', message="Please note that your password entries have to match and should be more than 8 characters.")
+        return render_template('register_fail.html', 
+                               message="Please note that your password entries have to match and should be more than 8 characters.",
+                               fail_type='Register', route='/register')
     password = sha512(password.encode('utf-8')).hexdigest()
     try:
         new_user = User(username=username, password=password, fav_colour=None, birth_date=None)
         db.session.add(new_user)
         db.session.commit()
     except:
-        return render_template('register_fail.html', message="Username in unavailable!")
+        return render_template('register_fail.html', message="Username in unavailable!",
+                               fail_type='Register', route='/register')
     new_user = User.query.filter_by(username = username).first()
     if new_user:
         login_user(new_user)
-    return render_template('sign_in_home.html', message= "Register Success", username=username)
+    return render_template('sign_in_home.html', message= "Register Success",
+                           username=username)
         
         
 
+# Return html file displaying users for post request and returns JSON of users for get request
 @main_app.route('/users', methods=["POST", "GET"])
 @login_required
 def users():
@@ -96,10 +101,13 @@ def users():
     elif request.method == "GET":
         output = {}
         for i, user in enumerate(result):
-            output[i] = [user.username]
+            output[i] = {'username': user.username,
+                         'fav_colour' : user.fav_colour,
+                         'birth_date': user.birth_date}
         return output
     
-    
+
+# Render page for user to edit their data
 @main_app.route('/edit_user_data')
 @login_required
 def edit_user_data():
@@ -109,6 +117,7 @@ def edit_user_data():
     return render_template('add_user_data.html', colour=colour)
 
 
+# Submit user data and add to database
 @main_app.route('/submitdata', methods=['POST'])
 @login_required
 def update_user_data():
@@ -121,7 +130,8 @@ def update_user_data():
     db.session.commit()
     return redirect('/home')
     
-    
+
+# Basic authentication for password
 def auth_password(password):
     if 8 <= len(password) <= 24:
         return True
